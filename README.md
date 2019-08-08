2ff8ac7d48d050e075e6939bd327e1b0e39bc654
raviasmitha <asmithachowdary1511@gmail.com>
2019-08-08 10:36:10 +0530

    data binding task scenario4

---
+++ README.md
@@ -0,0 +1,3 @@
+In this scenario, create two components, say component1 and component2. Component1 is a parent of component2.
+
+A user should be able to enter text in the .html template file of component2. The template of component1 should display the text.
---
+++ src/app/component1/component2/component2.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit ,Output , EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+
+  public name:string = "";
+  
+  @Output() MessageEvent = new EventEmitter<string>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick(value){
+    this.MessageEvent.emit(value);
+  }
+
+
+}
