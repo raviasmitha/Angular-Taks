1a84e589d7a7a2527c6039492adf3b18261bde77
raviasmitha <asmithachowdary1511@gmail.com>
2019-08-08 10:33:10 +0530

    data binding tasks scenario3

---
+++ README.md
@@ -0,0 +1,2 @@
+In this scenario, create two components, say component1 and component2. Component1 is a parent of component2.
+A user should be able to enter text in the .html template file of component1. The template of component2 should display the text.
---
+++ src/app/component1/component2/component2.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component {
+
+  constructor() { }
+
+  @Input() message:string;
+
+}
